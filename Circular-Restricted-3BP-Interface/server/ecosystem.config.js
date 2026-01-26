6744ce98f2ee30f57a8b5cd0d14f0f83a38ce16c
dalejandroabel <79865621+dalejandroabel@users.noreply.github.com>
2026-01-26 16:31:53 -0500

    Config file

---
+++ Circular-Restricted-3BP-Interface/server/ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "crtbp_api",
+      script: "index.js",
+
+      instances: 1,     
+      exec_mode: "fork",
+
+      autorestart: true,
+      watch: false,
+
+      max_memory_restart: "500M",
+
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001
+      },
+
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+      log_file: "logs/combined.log",
+      time: true
+    }
+  ]
+};
